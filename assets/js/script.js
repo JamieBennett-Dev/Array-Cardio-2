6945c3bc63b0d43ec0162f3893765da725c98a8c
Jamie Bennett <jbennett.130602@gmail.com>
2024-05-25 18:45:46 +0000

    first two tasks complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,26 @@ const people = [
     { name: 'Lux', year: 2015 }
   ];
 
-  const comments = [
+const comments = [
     { text: 'Love this!', id: 523423 },
     { text: 'Super good', id: 823423 },
     { text: 'You are the best', id: 2039842 },
     { text: 'Ramen is my fav food ever', id: 123523 },
     { text: 'Nice Nice Nice!', id: 542328 }
   ];
+
+// some and every checks
+// Array.prototype.some() // is at least one person 19?
+const isAdult = people.some(person => ((new Date()).getFullYear()) - person.year >= 19);
+console.log(isAdult); // returns true
+
+// Array.prototype.every() // is everyone 19?
+const allAdults = people.every(person => ((new Date()).getFullYear()) - person.year >= 19);
+console.log(allAdults); // returns false
+
+// Array.prototype.find()
+// Find is like filter, but instead returns just like the one you are looking for
+// Find the comment with the ID of 823423
+const comment = comments.find(comment => comment.id === 823423);
+console.log(comment); // returns item
+
